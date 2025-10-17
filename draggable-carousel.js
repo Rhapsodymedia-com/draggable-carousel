f8925e63f84266c24ef8bd5568e124d67cf17987
Radek <50740828+tomaszew95@users.noreply.github.com>
2025-10-17 14:14:25 +0200

    0.96.2

--- draggable-carousel.js
+++ draggable-carousel.js
@@ -238,7 +238,7 @@
                                         const viewSpace = this.setup.viewport[set.dimensions[s]]
                                         const startPoint = parseFloat(element.style.getPropertyValue(set.coordinates[s]))
                                         const centerPoint = parseFloat(element.style.getPropertyValue(set.dimensions[s]))/2
-                                        let snapPoint = viewSpace/2 - (startPoint + centerPoint) - this.setup.startingPoint[set.axises[s]]
+                                        let snapPoint = viewSpace/2 - (startPoint + centerPoint) + this.setup.startingPoint[set.axises[s]]
                                         let coord = parseFloat(this.mainElement.style.getPropertyValue(set.coordinates[s]))
                                         console.log(viewSpace/2, startPoint , centerPoint , this.setup.startingPoint[set.axises[s]])
                                         console.log(snapPoint, this.setup.range[set.dimensions[s]], coord)
